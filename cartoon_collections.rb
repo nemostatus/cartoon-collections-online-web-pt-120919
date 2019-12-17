f517dc64b625d7c2fc48a9acf4ea901fdba6fa87
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-17 22:07:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,4 @@ end
   long_planeteer_calls(["puff", "go", "two"])
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
