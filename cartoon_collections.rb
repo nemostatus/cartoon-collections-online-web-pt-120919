ac0b96c171bb9480355bb56e1dde3b9867e3c01c
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-17 22:06:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,9 @@ summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
 
 def long_planeteer_calls(short_words)
 
-   short_words.any? { |word| word.length > 4}
+   short_words.any?{ |word| word.length > 4}
 end
-end
+
   long_planeteer_calls(["puff", "go", "two"])
 end
 long_planeteer_calls(["puff", "go", "two"])
